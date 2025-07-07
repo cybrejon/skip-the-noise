0d6457a653467d5cf4b63c820c7b6fadd359e270
Martin Matovu <matovius@proton.me>
2025-07-07 15:49:46 +0300

    refactor: restructure logo component to make it scale according to scroll

--- src/components/ui/Logo.svelte
+++ src/components/ui/Logo.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
+  // type Variant = "primary" | "normal" | "symbol";
 
-  type Variant = 'primary' | 'normal' | 'symbol';
+  interface Props {
+    // variant?: Variant;
+    scrolled?: boolean;
+  }
 
-  let { variant = 'primary' } = $props<{ variant?: Variant }>()
+  let { scrolled = $bindable(false) }: Props = $props();
 </script>
 
-{#if variant === 'primary'}
-<h1><span>Skip The Noise</span><br />Media</h1>
-{/if}
+<div
+  class="{scrolled
+    ? 'text-base'
+    : 'text-lg'} leading-none tracking-tight font-manrope font-extrabold pointer-events-none select-none"
+>
+  <span>Skip The Noise</span><br /><span>Media</span>
+</div>
 
-{#if variant === 'normal'}
+<!-- {#if variant === "primary"}
   <p><span>Skip The Noise</span><br />Media</p>
-{/if}
-
-{#if variant === 'symbol'}
-  <!-- a glyph of the branding here instead of text, going to propose them a brand guideline after this project lol -->
+{:else if variant === "normal"}
   <p><span>Skip The Noise</span><br />Media</p>
-{/if}
+{:else if variant === "symbol"}
+  <!- a glyph of the branding here instead of text, going to propose them a brand guideline after this project lol ->
+  <p>STNM</p>
+{/if} -->
