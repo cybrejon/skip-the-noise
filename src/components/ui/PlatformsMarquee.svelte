f5e60d336b158733c9e1a590c03692980c3c94c3
Martin Matovu <matovius@proton.me>
2025-07-10 19:25:36 +0300

    feat: add `PlatformMarquee` component

---
+++ src/components/ui/PlatformsMarquee.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+  let marquee: HTMLDivElement;
+  let isAnimated: boolean = $state(false);
+  const platforms: string[] = [
+    "facebook",
+    "instagram",
+    "google",
+    "youtube",
+    "tiktok",
+    "pinterest",
+    "reddit",
+    "linkedin",
+    "spotify",
+    "amazon",
+    "twitter",
+  ];
+
+  $effect(() => {
+    if (marquee) {
+      if (!window.matchMedia("(prefers-reduced-motion: reduce").matches) {
+        setTimeout(() => {
+          isAnimated = true;
+        }, 2000);
+      }
+
+      setTimeout(() => {
+        marquee.style.visibility = "visible";
+        marquee.style.opacity = "1";
+      }, 2000);
+    }
+  });
+</script>
+
+<div
+  class="marquee"
+  style="opacity: 0; visibility: hidden;"
+  data-is-animated={isAnimated}
+  bind:this={marquee}
+>
+  <ul
+    class="scroller h-full flex justify-center items-center gap-15 px-0 py-12"
+  >
+    {#each platforms as platform}
+      <li class="">
+        {@render PlatformLogo(platform)}
+      </li>
+    {/each}
+    {#each platforms as platform}
+      <li class="">
+        {@render PlatformLogo(platform)}
+      </li>
+    {/each}
+  </ul>
+</div>
+
+{#snippet PlatformLogo(platform: string)}
+  <img
+    src="/platforms/{platform}.svg"
+    alt={platform}
+    class="size-30 aspect-square"
+  />
+{/snippet}
+
+<style>
+  @reference "../../app.css";
+
+  .marquee {
+    @apply w-full h-full select-none;
+    &[data-is-animated="true"] {
+      mask: linear-gradient(
+        90deg,
+        transparent,
+        white 10%,
+        white 90%,
+        transparent
+      );
+
+      & > .scroller {
+        @apply will-change-transform w-max;
+
+        animation: playMarquee 30s linear infinite forwards;
+        animation-play-state: running;
+      }
+    }
+
+    &:hover > .scroller {
+      animation-play-state: paused;
+    }
+  }
+
+  @keyframes playMarquee {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(calc(-50% - 1.875rem));
+    }
+  }
+</style>
