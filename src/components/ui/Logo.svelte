07c676ad35016bc65abe2dfa6e486eac5e51b655
cybrejon <cybrejon.senpai@gmail.com>
2025-07-12 23:47:34 +0800

    chore: remove unused code

--- src/components/ui/Logo.svelte
+++ src/components/ui/Logo.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  // type Variant = "primary" | "normal" | "symbol";
-
   interface Props {
-    // variant?: Variant;
     scrolled?: boolean;
   }
 
@@ -16,12 +13,3 @@
 >
   <span><span class="text-secondary-500">&lt;Skip</span>TheNoise</span><br /><span>media<span class="text-secondary-500">&gt;</span></span>
 </div>
-
-<!-- {#if variant === "primary"}
-  <p><span>Skip The Noise</span><br />Media</p>
-{:else if variant === "normal"}
-  <p><span>Skip The Noise</span><br />Media</p>
-{:else if variant === "symbol"}
-  <!- a glyph of the branding here instead of text, going to propose them a brand guideline after this project lol ->
-  <p>STNM</p>
-{/if} -->
